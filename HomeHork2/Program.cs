87cf3b95f900ccc216c0db17094a30d09094fc9a
vvg88 <grishaninv.v@gmail.com>
2017-05-25 22:12:37 +0300

    HomeWork2 Реализован вектор

---
+++ HomeHork2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector<int> a = new Vector<int>(1, 2);
+            Console.WriteLine($"Вектор а: {a}");
+            Vector<int> b = new Vector<int>(1, 2);
+            Console.WriteLine($"Вектор b: {b}");
+            Console.WriteLine($"Векторы а и b {(a == b ? "равны" : "не равны")}");
+            var c = a + b;
+            Console.WriteLine($"Вектор а + b: {a + b}");
+
+
+        }
+    }
+}
---
+++ HomeHork2/Vector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Vector<T> where T : struct
+    {
+        public T X { get; private set; }
+
+        public T Y { get; private set; }
+
+        public double Length
+        {
+            get
+            {
+                dynamic x = X;
+                dynamic y = Y;
+                return Math.Sqrt(x * x + y * y);
+            }
+        }
+
+        public Vector(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{X};\t{Y}";
+        }
+
+        public static Vector<T> operator +(Vector<T> leftOp, Vector<T> righOp)
+        {
+            dynamic lx = leftOp.X;
+            dynamic ly = leftOp.Y;
+            return new Vector<T>(lx + righOp.X, ly + righOp.Y);
+        }
+
+        public static Vector<T> operator +(Vector<T> leftOp, T righOp)
+        {
+            dynamic lx = leftOp.X;
+            dynamic ly = leftOp.Y;
+            return new Vector<T>(lx + righOp, ly + righOp);
+        }
+
+        public static bool operator ==(Vector<T> leftOp, Vector<T> righOp)
+        {
+            return leftOp.Length == leftOp.Length;
+        }
+
+        public static bool operator !=(Vector<T> leftOp, Vector<T> righOp)
+        {
+            return leftOp.Length != leftOp.Length;
+        }
+    }
+}
