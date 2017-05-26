d8ff89e2f53484ceccadd19abdb4d7ac0a125d04
Vladimir Grishanin <vvg@neurosoft.ru>
2017-05-26 18:39:49 +0300

    HomeWork2 Добавлен генератор векторов и факториал на стеке

--- HomeHork2/Program.cs
+++ HomeHork2/Program.cs
@@ -8,6 +8,13 @@ namespace HomeWork2
 {
     class Program
     {
+
+        struct ms
+        {
+            public int a;
+            public int b;
+        }
+
         static void Main(string[] args)
         {
             Vector<int> a = new Vector<int>(1, 2);
@@ -15,10 +22,23 @@ namespace HomeWork2
             Vector<int> b = new Vector<int>(1, 2);
             Console.WriteLine($"Вектор b: {b}");
             Console.WriteLine($"Векторы а и b {(a == b ? "равны" : "не равны")}");
-            var c = a + b;
-            Console.WriteLine($"Вектор а + b: {a + b}");
+            Console.WriteLine($"Вектор а + b: {a + b}\n");
 
+            Console.WriteLine("Сгенерировать массив векторов:");
+            var vectGen = new VectorGenerator<int>(5);
+            foreach (var vect in vectGen)
+                Console.WriteLine(vect);
 
+            Console.WriteLine($"5!: {Factorial(3)}");
+        }
+
+        private static double Factorial(int n)
+        {
+            Stack<double> stack = new Stack<double>();
+            stack.Push(1);
+            for (int i = 1; i <= n; i++)
+                stack.Push(stack.Pop() * i);
+            return stack.Pop();
         }
     }
 }
---
+++ HomeHork2/VectorGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class VectorGenerator<T> : IEnumerable<Vector<T>> where T : struct
+    {
+        private List<Vector<T>> vectors;
+
+        public VectorGenerator(int vectNum)
+        {
+            vectors = new List<Vector<T>>(vectNum);
+            var randomGen = new Random();
+            for (int i = 0; i < vectNum; i++)
+            {
+                dynamic x = randomGen.Next(-10, 10);
+                dynamic y = randomGen.Next(-10, 10);
+                vectors.Add(new Vector<T>(x, y));
+            }
+        }
+
+        public IEnumerator<Vector<T>> GetEnumerator()
+        {
+            return vectors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
